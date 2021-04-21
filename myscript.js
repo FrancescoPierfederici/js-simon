d72f174f6742743f96daa647374e033767c8c9f2
FrancescoPierfederici <francescopierf@gmail.com>
2021-04-21 18:56:20 +0200

    commit finale

--- myscript.js
+++ myscript.js
@@ -12,13 +12,14 @@
 // SCRIVO 3 ARRAY PER TUTTI I NUMERI
 var numeriGenerati = [];
 var numeriUtente = [];
+var numeriUtenteGiusti = [];
 var numeriUtenteErrati = [];
 var secondi = 10000;   //30000
 
 // APRO UN CICLO FOR PER GENERARE 5 NUMERI RANDOM E LI PUSHO DENTRO ALL'ARRAY 
 for (var i = 0; i < 5; i++) {
-    var element = 5[i];
-    element = Math.floor(Math.random() * 100);
+    var element = Math.floor(Math.random() * 100);
+//INSERISCO I NUMERI RANDOM NELL'ARRAY NUMERI GENERATI
     numeriGenerati.push(element);
 }
 // VERIFICO I NUMERI GENERATI
@@ -27,18 +28,38 @@ console.log(numeriGenerati);
 alert(numeriGenerati);
 
 
+// CREO UN TIMER DI 30 SECONDI CON SE UNA FUNZIONE
 
+setTimeout(function () {
+//CHIEDO CON UN PROMPT CON I NUMERI DA INSERIRE DANDO PER SCONTATO CHE NON INSERISCA COSE DIVERSE DA NUMERI
+    var numeriUtenteIns = prompt("Inserisci tutti i numeri visti precedentemente mettendoli separati da una virgola...").split(",");
 
-setTimeout(function(){
-var numeriUtenteIns = parseInt(prompt("Inserisci tutti i numeri visti precedentemente..."));
-console.log(numeriUtenteIns)
-// if (numeriDigitati === numeriGenerati){
-//     numeriUtente.push(numeriDigitati);
-// }else if(numeriDigitati !== numeriGenerati) {
-//     numeriUtenteErrati.push(numeriDigitati)
-// }
-// console.log(numeriUtenteErrati);
-// console.log(numeriUtente);
+// APRO UN CICLO FOR PER CONVERTIRE LE STRINGHE IN NUMERI
+    for (var j = 0; j < numeriUtenteIns.length; j++) {
+        var numeriConvertiti = parseInt(numeriUtenteIns[j]);
+        console.log(numeriConvertiti);
+        numeriUtente.push(numeriConvertiti);
+    }
+
+    console.log(numeriUtente);
+
+//APRO UN CICLO FOR PER COMPARARE DUE ARRAY 
+    for (var y = 0; y < numeriGenerati.length; y++) {
+        var elementTwo = numeriGenerati[y];
+
+        if (numeriUtente.includes(elementTwo)) {
+            numeriUtenteGiusti.push(elementTwo);
+        } else {
+            numeriUtenteErrati.push(elementTwo)
+
+        }
+
+    }
+// RIPRODUCO IN CONSOLE I NUMERI PRESI E NON PRESI
+    console.log("I numeri azzeccati sono: " + numeriUtenteGiusti);
+    console.log("I numeri non inseriti sono: " + numeriUtenteErrati);
+
+  
 
 }, secondi);
 
@@ -46,8 +67,7 @@ console.log(numeriUtenteIns)
 
 
 
-
-
+//
 
 
 
